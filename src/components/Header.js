af1e0f9b49c58e532f0194784e3a192779e0c2e6
Pranshu321 <pranshujain0111@gmail.com>
2022-07-10 01:11:59 +0530

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <h1>Hangman</h1>
+      <h1><span><img src='./hangman.png' alt='logo' width="44" /></span>   Hangman</h1>
       <p style={{color: "#20a7ff"}}>Find the Hidden Word & Unleash Your Vocabluary ▶</p>
     </>
   )
